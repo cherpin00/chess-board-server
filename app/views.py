8b1ed581dd18483112fa5c20dfc8d2cb9f12ffdd
caleb <caleb.herpin@gmail.com>
2021-03-31 11:27:54 -0500

    Basic javascript chess board

---
+++ app/views.py
@@ -0,0 +1,33 @@
+import logging
+
+import chess
+from flask import render_template, send_from_directory, request
+from flask.json import jsonify
+
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/board')
+def redner_board():
+    board = chess.Board()
+    # return render_template('home.html', fen=board.fen().split(" ")[0])
+    return render_template(
+        'home.html', fen="rnbqkbnr/pppppppp/8/2Q1P3/8/6P1/PPPP1P1P/RNB1KBNR")
+
+
+@app.route('/img/<path:filename>')
+def base_static(filename):
+    return send_from_directory(app.root_path + '/static/img/', filename)
+
+
+@app.route('/updateBoard', methods=["POST"])
+def update_board():
+    ren = request.json
+    print(ren)
+    return jsonify(success=True), 200
